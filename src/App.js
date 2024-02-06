b03c2682d802ea6157ffeceadcaf6ffb330271bb
dev-ajitkadam <kadamajit2002@gmail.com>
2024-02-06 21:52:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='container bg-blue-500'>
+      App Components
+
+    </div>
+    </>
+  );
+}
+
+export default App;
