ada717745b254d9ca8db924f1778530380029624
dev-ajitkadam <kadamajit2002@gmail.com>
2024-02-09 16:06:13 +0530

    name

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ function App() {
   return (
     <BrowserRouter>
     <NavBar/>
+    <div className="max-w-screen-md mx-auto pt-20">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/article" element={<Article />} />
+        <Route path="/article/:name" element={<Article />} />
         <Route path="/articles-List" element={<ArticlesList />} />
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/Pages/Article.js
+++ src/Pages/Article.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 function Article() {
+    const name= useParams();
   return (
-    <div>Article PAGE</div>
+    <div className='mb-20'>
+    <div className='sm:text-4xl text-2xl font-bold mt-6 text-gray-900'>Article PAGE</div>
+    <h1>this is a {name.name} article</h1>
+    </div>
   )
 }
 
